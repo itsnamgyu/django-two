e2891a323241caa8460095af72ee5a7ee3f9e2c1
Namgyu Ho <itsnamgyu@gmail.com>
2018-01-20 05:53:00 +0000

    Added poll models & applied to admin

--- django_two/django_two/settings.py
+++ django_two/django_two/settings.py
@@ -55,6 +55,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     # These are all apps some of which have their own models!
     # That's why you need the initial migrate!
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
