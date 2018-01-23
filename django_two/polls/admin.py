1d278174b8c25b0781b71e6b2ada45caf6621b7b
Namgyu Ho <itsnamgyu@gmail.com>
2018-01-23 07:02:36 +0000

    Customized admin

--- django_two/polls/admin.py
+++ django_two/polls/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None,               {'fields': ['question_text']}),
+            ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+            ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+
+# Without the QuestionAdmin argument, this just creates the default admin page
+# with fields for all attributes of Questions with no fields for choices.
+admin.site.register(Question, QuestionAdmin)
+
 admin.site.register(Choice)
--- django_two/polls/models.py
+++ django_two/polls/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -7,6 +9,14 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=5) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
